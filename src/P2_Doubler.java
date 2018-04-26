b9008700c9ac99b0111a367cb91d94aa6c9f77db
Destiny Goubeaux <dgoubeaux1@live.maryville.edu>
2018-04-26 12:55:57 -0500

    Changes

--- src/P2_Doubler.java
+++ src/P2_Doubler.java
@@ -1,3 +1,6 @@
+import java.util.*;
+
+
 /*
 	ISYS 320
 	Name(s):
@@ -7,7 +10,19 @@
 public class P2_Doubler {
 
 	public static void main(String[] args) {
-
+			
+		Scanner doubler = new Scanner(System.in);
+			
+			
+			System.out.print("What is your number? ");
+			int userNumber = doubler.nextInt();
+			
+			
+			System.out.println("That number doubled is ");
+			System.out.print(userNumber * 2);
+			
+			
+		}
 	}
 
-}
+
--- src/P3_EchoChamber.java
+++ src/P3_EchoChamber.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /*
 	ISYS 320
 	Name(s):
@@ -8,6 +10,14 @@ public class P3_EchoChamber {
 
 	public static void main(String[] args) {
 
+		Scanner phrase = new Scanner(System.in);
+		
+		System.out.print("What is your phrase? ");
+		String input = phrase.nextLine();
+		
+		System.out.print("How many times should i repear it? ");
+		int number = phrase.nextInt();
+	
 	}
 
 }
